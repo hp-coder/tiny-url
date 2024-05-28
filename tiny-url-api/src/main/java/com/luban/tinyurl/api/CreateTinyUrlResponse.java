680ffeac56ad208a0b18399dacb71a6bd179e125
hupeng <hup_dev@outlook.com>
2024-05-28 15:27:00 +0800

    No records allowed.

--- tiny-url-api/src/main/java/com/luban/tinyurl/api/CreateTinyUrlResponse.java
+++ tiny-url-api/src/main/java/com/luban/tinyurl/api/CreateTinyUrlResponse.java
@@ -1,9 +1,17 @@
 package com.luban.tinyurl.api;
 
 import com.luban.common.base.model.Response;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author hp
  */
-public record CreateTinyUrlResponse(String code) implements Response {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateTinyUrlResponse implements Response {
+
+    private String code;
 }
